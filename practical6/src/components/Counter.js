f6254d6ed678ff2c19889a957156ad82af54ceed
tanishaagarwal1234 <tanishaagarwal.it22@scet.ac.in>
2025-01-30 12:22:05 +0000

    practical6

---
+++ practical6/src/components/Counter.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import './counter.css'
+function Counter() {
+    const [counter, setCounter] = useState(0)
+    const handleInrement = () => {
+        setCounter(counter+1);
+    }
+    const handleDerement = () => {
+        setCounter(counter-1)
+    }
+    useEffect(()=>{
+        document.title = `Counter : ${counter}`  
+    },[counter])
+  return (
+    <div className='box'>
+      <h1>Counter : {counter}</h1>
+      <button onClick={handleInrement}>Increment Counter</button>
+      <br/>
+      <button onClick={handleDerement}>Decrement Counter</button>
+    </div>
+  )
+}
+export default Counter
